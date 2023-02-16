13078d68823e362c7350a426ef7ba058328d9e42
Nitish Kumar Ojha <120451656+NITISHOJHA28@users.noreply.github.com>
2023-02-16 07:56:51 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+	public static void main(String[] args) {
+		RWOnly rwOnly = new RWOnly();
+		//rwOnly.name;
+		//Unresolved compilation problems
+		rwOnly.setName("Harshad");
+		System.out.println(rwOnly.getName());
+	}
 }
